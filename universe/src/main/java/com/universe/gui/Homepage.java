b6e63c9e2765c16c289988f2f126a4ff5d36d64e
Kosy Oraka <kosisochukwuoraka@gmail.com>
2024-11-14 20:09:57 -0500

    Latest Update

--- universe/src/main/java/com/universe/gui/Homepage.java
+++ universe/src/main/java/com/universe/gui/Homepage.java
@@ -43,6 +43,7 @@ public class Homepage extends JFrame {
     public Homepage() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 900, 600);
+        setResizable(false); // Prevents resizing of the window
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         contentPane.setLayout(null);
