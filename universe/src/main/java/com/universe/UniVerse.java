b3ec8e97d55f5026f9ef410dd08198d15dea4526
Timi Olajonlu <olajonlutimilehin@gmail.com>
2024-11-18 22:23:19 -0500

    update app

---
+++ universe/src/main/java/com/universe/UniVerse.java
@@ -0,0 +1,13 @@
+package com.universe;
+
+import com.universe.gui.SignUporIn;
+
+public class UniVerse {
+    public static void main(String[] args) {
+        // Initialize Firebase
+        FirebaseInitializer.initializeFirebase();
+
+        // Launch the SignUporIn GUI
+        SignUporIn.main(args);
+    }
+}
